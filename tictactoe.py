26aea48057fec0796cccd337a9019a1c4b62128e
99fugo <fugo99.00@gmail.com>
2020-04-26 17:17:09 -0400

    Implemented functions: play, actions, result, winner

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,8 @@ Tic Tac Toe Player
 """
 
 import math
+from copy import deepcopy
+from collections import Counter
 
 X = "X"
 O = "O"
@@ -13,37 +15,70 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+    return [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
 
 
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    # Count state occurences in flattened board
+    counts = Counter([x for row in board for x in row])
+
+    # Return "X" if odd number of empty squares
+    return "X" if counts[None] % 2 == 0 else "O"
+
+    # or, more succinct, but clearer and/or faster?
+    # return "O" if [x for row in board for x in row].count(None) % 2 else "X"
+    # return "O" if Counter([x for row in board for x in row])[None] % 2 else "X"
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    return set(
+        [
+            (i, j)
+            for i, row in enumerate(board)
+            for j, col in enumerate(row)
+            if col == None
+        ]
+    )
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+
+    # Make deep copy of the board
+    next_board = deepcopy(board)
+
+    # Add next move to the next board
+    i, j = action
+    next_board[i][j] = player(board)
+
+    return next_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    rotated_board = list(zip(board))
+
+    # assuming a move has been made
+    for b in (board, rotated_board):
+
+        # check rows/columns
+        for line in b:
+            if len(set(line)) == 1:
+                return line[0]
+
+        # check diagonals
+        if len(set([b[i][i] for i in range(3)])) == 1:
+            return b[i][i]
 
 
 def terminal(board):
